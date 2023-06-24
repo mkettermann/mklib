67b0103ee3307d84db4ee7e5bef72be69f02bfff
MKettermann <ultramk10@gmail.com>
2023-06-24 20:26:09 -0300

    arquivo de exibicao de listagem

--- js/site.js
+++ js/site.js
@@ -1,5 +1,19 @@
 "use strict";
-const menuAbrir = (e) => {
+const menuAbrir = async (e) => {
     console.log(mk.QdataGet(e, "go"));
-    window.location = "/" + mk.QdataGet(e, "go");
+    let destino = mk.QdataGet(e, "go");
+    if (destino != null) {
+        let html;
+        let retorno = await mk.http(destino, mk.t.G, mk.t.H);
+        if (retorno != null) {
+            console.log("ok");
+            mk.Q("main").innerHTML = retorno;
+        }
+        else {
+            console.log("ERRO");
+        }
+    }
+    else {
+        console.log("Destino Nulo");
+    }
 };
--- ts/site.ts
+++ ts/site.ts
@@ -1,4 +1,16 @@
-const menuAbrir = (e: HTMLElement) => {
+const menuAbrir = async (e: HTMLElement) => {
 	console.log(mk.QdataGet(e, "go"));
-	window.location = "/" + mk.QdataGet(e, "go");
+	let destino = mk.QdataGet(e, "go");
+	if (destino != null) {
+		let html;
+		let retorno = await mk.http(destino, mk.t.G, mk.t.H);
+		if (retorno != null) {
+			console.log("ok");
+			mk.Q("main").innerHTML = retorno;
+		} else {
+			console.log("ERRO");
+		}
+	} else {
+		console.log("Destino Nulo");
+	}
 };
