e7e1c2f42c5b23d14e7003d8b7ea6a559eea8179
mkettermann <ultramk10@gmail.com>
2023-07-13 16:46:25 -0300

    fix ts

--- js/listagemMk.js
+++ js/listagemMk.js
@@ -1,13 +1,3 @@
 "use strict";
 
 var lista = new Mk("/data/usersExemplo.json", ".tabela1");
-
-// mk.iniciarGetList("/data/usersExemplo.json", true);
-// mk.GerarAoSort();
-// mk.antesDePopularTabela = () => {
-//     mk.exibePaginado.forEach((o) => {
-//         o["mDataUltimoAcesso"]
-//             ? (o["mDataUltimoAcesso"] = mk.mkYYYYMMDDtoDDMMYYYY(o["mDataUltimoAcesso"]))
-//             : null;
-//     });
-// };
