811e4a04ed7ba340110b8ae3a12709de51dd5b6a
MKettermann <ultramk10@gmail.com>
2023-07-13 23:44:01 -0300

    movi getlist

--- js/listagemMk.js
+++ js/listagemMk.js
@@ -3,11 +3,6 @@
 // mk.aoReceberDados = (data) => {
 // 	console.log("oi");
 // };
-
-var lista = new Mk(
-	"/data/usersExemplo.json",
-	".tabela1",
-	"#modelo",
-	mk.aoReceberDados
-);
+var lista = new Mk();
+//var lista = new Mk("/data/usersExemplo.json", ".tabela1", "#modelo");
 lista.ativarSort();
