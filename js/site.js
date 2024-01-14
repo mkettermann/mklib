e4004178ae238d86a0a200a1fd6fb0705c6858e7
MKettermann <ultramk10@gmail.com>
2024-01-14 12:20:07 -0300

    with cookie

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
-const menuAbrir = async (e) => {
+const menuAbrir = async (t) => {
+	let e = mk.Q("#" + t);
 	let destino = mk.QdataGet(e, "go");
 	if (destino != null) {
 		let pac = await mk.get.html("./html/" + destino);
@@ -11,8 +12,14 @@ const menuAbrir = async (e) => {
 	} else {
 		console.log("Destino Nulo");
 	}
+	setCookie("ultima", t);
 };
-menuAbrir(mk.Q(".MenuLink.inicio"));
+let ultima = getCookie("ultima");
+if (ultima == "") {
+	ultima = "zone"
+	setCookie("ultima", ultima);
+}
+menuAbrir(ultima);
 
 /**Confirmar */
 const aoIncrementar = async (e) => {
@@ -25,3 +32,28 @@ const aoIncrementar = async (e) => {
 		r ? edit(alvo, Number(alvo?.value) + 1) : console.log("Você não confirmou");
 	});
 };
+
+
+/* Cookies */
+function setCookie(name, value, days) {
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+function eraseCookie(name) {
+	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
