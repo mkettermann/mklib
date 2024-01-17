580ce4c28edb04047fea626e029e5180b0761bed
MKettermann <ultramk10@gmail.com>
2024-01-16 22:10:09 -0300

    uuid

--- js/mk.js
+++ js/mk.js
@@ -4648,6 +4648,10 @@ class mk {
             });
         });
     };
+    // UUIDV4 Gerador de identificador unico
+    static uuid = () => {
+        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+    };
     // LOGGER (Meta programming)
     static log = (o, name) => {
         // Função para debugar determinado objeto.
--- ts/mk.ts
+++ ts/mk.ts
@@ -4872,6 +4872,13 @@ class mk {
 		});
 	};
 
+	// UUIDV4 Gerador de identificador unico
+	static uuid: any = () => {
+		return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+			(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+		);
+	}
+
 	// LOGGER (Meta programming)
 	static log: any = (o: any, name: any) => {
 		// Função para debugar determinado objeto.
