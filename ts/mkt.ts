d5aa6b4cc87ec8e9ed9a978aa0f85a3bc130a658
mkettermann <ultramk10@gmail.com>
2024-01-23 09:30:47 -0300

    fix

--- ts/mkt.ts
+++ ts/mkt.ts
@@ -2609,7 +2609,7 @@ Object.defineProperty(mkt, "CarregarON", {
 		if (mkt.Q("body .CarregadorMkBlock") == null) {
 			let divCarregadorMkBlock = document.createElement("div");
 			divCarregadorMkBlock.className = "CarregadorMkBlock";
-			let divCarregadorMk = document.createElement("div");
+			let divCarregadormkt = document.createElement("div");
 			divCarregadormkt.className = "CarregadorMk";
 			let buttonCarregadorMkTopoDireito = document.createElement("button");
 			buttonCarregadorMkTopoDireito.className = "CarregadorMkTopoDireito";
@@ -2617,7 +2617,7 @@ Object.defineProperty(mkt, "CarregarON", {
 			buttonCarregadorMkTopoDireito.setAttribute("onClick", "mkt.CarregarOFF()");
 			buttonCarregadorMkTopoDireito.innerHTML =
 				"<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' viewBox='0 0 16 16'><path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z'/></svg>";
-			divCarregadorMkBlock.appendChild(divCarregadorMk);
+			divCarregadorMkBlock.appendChild(divCarregadormkt);
 			divCarregadorMkBlock.appendChild(buttonCarregadorMkTopoDireito);
 			document.body.appendChild(divCarregadorMkBlock);
 		}
