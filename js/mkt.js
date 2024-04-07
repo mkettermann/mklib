66ba09b00f8060af2d3ba8e78070570728a7fd93
MKettermann <ultramk10@gmail.com>
2024-04-06 22:15:32 -0300

    fix

--- js/mkt.js
+++ js/mkt.js
@@ -96,7 +96,7 @@ class mktm {
             this.field = o.field;
         }
         else {
-            this.field = '<' + this.tag + ' name="' + this.k + '" value="' + this.v + '" class="' + this.classes + '" data-mkfformato="' + this.filtroFormato + '" ' + this.atr + '>';
+            this.field = "<" + this.tag + " name='" + this.k + "' value='" + this.v + "' class='" + this.classes + "' data-mkfformato='" + this.filtroFormato + "' " + this.atr + ">";
             if (this.tag != "input") {
                 this.field += "</" + this.tag + ">";
             }
