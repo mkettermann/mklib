146b19686eb51a145ec842c1013b4798a206fac8
MKettermann <ultramk10@gmail.com>
2024-04-22 22:06:32 -0300

    fix

--- js/mkt.js
+++ js/mkt.js
@@ -6868,8 +6868,8 @@ class mkBot extends HTMLElement {
             }
             else {
                 retornar = `<picture>
-					<source media="(min-width:300px)" srcset="${this.config.dados}" class="imgEmpresa">
-					<img src="${this.config.inicial}" class="imagem">
+					<source media="(min-width:300px)" srcset="${this.config.dados}" class="imagem" alt="Imagem" onerror="this.onerror=null;this.src='data:image/svg+xml<svg xmlns='http://www.w3.org/2000/svg' class='imagem' part='imagem' viewBox='0 0 24 24' fill='none'><path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M21 12v7a2 2 0 0 1-2 2h-3m5-9c-6.442 0-10.105 1.985-12.055 4.243M21 12v-1.5M3 16v3a2 2 0 0 0 2 2v0h11M3 16V5a2 2 0 0 1 2-2h8M3 16c1.403-.234 3.637-.293 5.945.243M16 21c-1.704-2.768-4.427-4.148-7.055-4.757M8.5 7C8 7 7 7.3 7 8.5S8 10 8.5 10 10 9.7 10 8.5 9 7 8.5 7zM19 2v3m0 3V5m0 0h3m-3 0h-3'/></svg>';">
+					<img src="${this.config.inicial}" class="imagem" alt="Imagem" />
 				</picture>`;
             }
             if (!this.config.clicavel) {
