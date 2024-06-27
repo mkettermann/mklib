853c9b01fd4034cf28f9b6d55b251773aa49e6d0
MKettermann <ultramk10@gmail.com>
2024-06-27 17:02:00 -0300

    fix confirmador

--- ts/mkt.ts
+++ ts/mkt.ts
@@ -2883,7 +2883,7 @@ class mkt {
 		p: any = null
 	) => {
 		// p { corSim: "bVerde", corNao: "bCinza"}
-		let possiveisBotoes = ["bCinza", "bVermelho", "bVerde"];
+		//let possiveisBotoes = ["bCinza", "bVermelho", "bVerde"];
 		let corSim = "bVerde";
 		if (p?.corSim != undefined) corSim = p.corSim;
 		let corNao = "bCinza";
@@ -2899,69 +2899,54 @@ class mkt {
 		let classContainer = "";
 		if (p?.classContainer != undefined) classContainer = p.classContainer;
 		return new Promise((r) => {
-			function verficiarResposta() {
+			// Criar se não houver
+			if (!document.querySelector(".mkConfirmadorBloco")) {
+				mkt.Q("body").insertAdjacentHTML("beforeEnd", `<div class="mkConfirmadorBloco microPos5 oculto">
+					<div class="mkConfirmadorFora" onclick="mkt.mkConfirmaFuncaoFora()"></div>
+					<div class="mkConfirmadorArea microPos5 tb fsb ${classContainer}">
+						<div class="mkConfirmadorTitulo">
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM5.495 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z"></path></svg>
+							<span class="mkConfirmadorTituloInner">${titulo}</span>
+						</div>
+						<div class="mkConfirmadorTexto">${texto}</div>
+						<div class="mkConfirmadorBotoes fsb">
+							<button class="bBotao icoSim ${corSim}" onclick="this.classList.add(true);">${emSim}</button>
+							<button class="bBotao icoNao ${corNao}" onclick="this.classList.add(true);">${emNao}</button>
+						</div>
+					</div>
+				</div>`);
+			}
+			// Limpeza de cores anteriores para setar o novo
+			// possiveisBotoes.forEach((s) => {
+			// 	mkt.QAll(".mkConfirmadorBloco .bBotao").forEach((botao: any) => {
+			// 		botao.classList.remove(s);
+			// 	});
+			// });
+			// Atualiza dados
+			mkt.Q(".mkConfirmadorBloco .mkConfirmadorArea").setAttribute("class", `mkConfirmadorArea microPos5 tb fsb ${classContainer}`);
+			mkt.Q(".mkConfirmadorBloco .mkConfirmadorTituloInner").innerHTML = titulo;
+			mkt.Q(".mkConfirmadorBloco .mkConfirmadorTexto").innerHTML = texto;
+			mkt.Q(".mkConfirmadorBloco .bBotao.icoSim").innerHTML = emSim;
+			mkt.Q(".mkConfirmadorBloco .bBotao.icoNao").innerHTML = emNao;
+			mkt.Q(".mkConfirmadorBloco .bBotao.icoSim").setAttribute("class", `bBotao icoSim ${corSim}`);
+			mkt.Q(".mkConfirmadorBloco .bBotao.icoNao").setAttribute("class", `bBotao icoNao ${corNao}`);
+			mkt.Q(".mkConfirmadorBloco").classList.remove("oculto");
+
+			const checkResposta = setInterval(() => {
 				let resposta = null;
-				if (mkt.Q(".mkConfirmadorBloco .mkConfirmadorArea .bBotao.icoSim.true"))
+				if (mkt.Q(".mkConfirmadorBloco .mkConfirmadorArea .bBotao.icoSim.true")) {
 					resposta = true;
-				if (mkt.Q(".mkConfirmadorBloco .mkConfirmadorArea .bBotao.icoNao.true"))
+				}
+				if (mkt.Q(".mkConfirmadorBloco .mkConfirmadorArea .bBotao.icoNao.true")) {
 					resposta = false;
+				}
 				if (resposta !== null) {
 					mkt.Q(".mkConfirmadorBloco .icoSim").classList.remove("true");
 					mkt.Q(".mkConfirmadorBloco .icoNao").classList.remove("true");
 					mkt.Q(".mkConfirmadorBloco").classList.add("oculto");
 					retornar(resposta);
 				}
-			}
-			if (!document.querySelector(".mkConfirmadorBloco")) {
-				let divMkConfirmarBloco = document.createElement("div");
-				let divMkConfirmarFora = document.createElement("div");
-				let divMkConfirmarArea = document.createElement("div");
-				let divMkConfirmarTitulo = document.createElement("div");
-				let divMkConfirmarTexto = document.createElement("div");
-				let divMkConfirmarBotoes = document.createElement("div");
-				let divMkConfirmarSim = document.createElement("button");
-				let divMkConfirmarNao = document.createElement("button");
-				divMkConfirmarBloco.className = "mkConfirmadorBloco microPos5";
-				divMkConfirmarFora.className = "mkConfirmadorFora";
-				divMkConfirmarArea.className =
-					"mkConfirmadorArea microPos5 tb fsb " + classContainer;
-				divMkConfirmarTitulo.className = "mkConfirmadorTitulo";
-				divMkConfirmarTexto.className = "mkConfirmadorTexto";
-				divMkConfirmarBotoes.className = "mkConfirmadorBotoes fsb";
-				divMkConfirmarSim.className = "bBotao icoSim " + corSim;
-				divMkConfirmarNao.className = "bBotao icoNao " + corNao;
-				divMkConfirmarTitulo.innerHTML = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' viewBox='0 0 16 16'>
-						<path d='M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM5.495 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z'/>
-					</svg><span>${titulo}</span>`;
-
-				divMkConfirmarTexto.innerHTML = texto;
-				divMkConfirmarSim.innerHTML = emSim;
-				divMkConfirmarNao.innerHTML = emNao;
-				divMkConfirmarFora.setAttribute("onclick", "mkt.mkConfirmaFuncaoFora()");
-				divMkConfirmarSim.setAttribute("onclick", "this.classList.add(true);");
-				divMkConfirmarNao.setAttribute("onclick", "this.classList.add(true);");
-				mkt.Q("body").appendChild(divMkConfirmarBloco);
-				divMkConfirmarBloco.appendChild(divMkConfirmarFora);
-				divMkConfirmarBloco.appendChild(divMkConfirmarArea);
-				divMkConfirmarArea.appendChild(divMkConfirmarTitulo);
-				divMkConfirmarArea.appendChild(divMkConfirmarTexto);
-				divMkConfirmarArea.appendChild(divMkConfirmarBotoes);
-				divMkConfirmarBotoes.appendChild(divMkConfirmarSim);
-				divMkConfirmarBotoes.appendChild(divMkConfirmarNao);
-			} else {
-				// Limpeza de cores anteriores
-				possiveisBotoes.forEach((s) => {
-					mkt.QAll(".mkConfirmadorBloco .bBotao").forEach((botao: any) => {
-						botao.classList.remove(s);
-					});
-				});
-				// Set das cores novas
-				mkt.Q(".mkConfirmadorBloco .bBotao.icoSim").setAttribute("class", `bBotao icoSim ${corSim}`);
-				mkt.Q(".mkConfirmadorBloco .bBotao.icoNao").setAttribute("class", `bBotao icoNao ${corNao}`);
-				mkt.Q(".mkConfirmadorBloco").classList.remove("oculto");
-				mkt.Q(".mkConfirmadorTexto").innerHTML = texto;
-			}
-			const checkResposta = setInterval(verficiarResposta, 100);
+			}, 100);
 			// Função de conclusão.
 			function retornar(resultado: boolean = false) {
 				clearInterval(checkResposta);
