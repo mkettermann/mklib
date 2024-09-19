da13a8c5da01c38db2d18e70ed6ec18317feee5b
MKettermann <ultramk10@gmail.com>
2024-09-19 13:22:39 -0300

    fix toNumber

--- js/mkt.js
+++ js/mkt.js
@@ -2647,7 +2647,7 @@ class mkt {
                 }
                 else {
                     valor = [...valor.toString()].filter(a => { return new RegExp(mkt.a.util.numeros[1]).test(a); }).join("").padStart(3, "0");
-                    valor = valor.slice(0, -(c.casas)) + "." + valor.slice(-(c.casas));
+                    valor = valor.slice(0, -(c.casas)) + (c.casas > 0 ? "." : "") + valor.slice(-(c.casas));
                 }
             }
             else if (mkt.classof(valor) == "Number") {
--- ts/mkt.ts
+++ ts/mkt.ts
@@ -3064,7 +3064,7 @@ class mkt {
 					valor = i + "." + d;
 				} else {
 					valor = [...valor.toString()].filter(a => { return new RegExp(mkt.a.util.numeros[1]).test(a) }).join("").padStart(3, "0")
-					valor = valor.slice(0, -(c.casas)) + "." + valor.slice(-(c.casas));
+					valor = valor.slice(0, -(c.casas)) + (c.casas > 0 ? "." : "") + valor.slice(-(c.casas));
 				}
 			} else if (mkt.classof(valor) == "Number") {
 				valor = valor.toFixed(c.casas); // <= Vira String, mas essa função apenas devolve Number
